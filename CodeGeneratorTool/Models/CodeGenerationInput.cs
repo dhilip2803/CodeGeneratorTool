81096ca3cef6fe862f947946d8332bbb41675f47
Dhilip Kumar M <8258251@ups.com>
2025-08-07 12:55:07 +0530

    Add project files.

---
+++ CodeGeneratorTool/Models/CodeGenerationInput.cs
@@ -0,0 +1,15 @@
+﻿namespace CodeGeneratorTool.Models
+{
+    public class CodeGenerationInput
+    {
+        public string ControllerName { get; set; }
+        public string ParameterName { get; set; }
+        public string ReturnType { get; set; }
+        public string ParameterType { get; set; }
+        public bool IsAsync { get; set; }
+        public string RouteHeader { get; set; }
+        public string RouteName { get; set; }
+        public string ParameterSource { get; set; } 
+    }
+
+}
---
+++ CodeGeneratorTool/Services/CodeBuilderService.cs
@@ -0,0 +1,22 @@
+﻿using CodeGeneratorTool.Models;
+
+namespace CodeGeneratorTool.Services
+{
+    public class CodeBuilderService
+    {
+        public string GenerateController(CodeGenerationInput input)
+        {
+            var asyncKeyword = input.IsAsync ? "async Task" : "ActionResult";
+            var methodName = input.IsAsync ? "Get" + input.ControllerName + "Async" : "Get" + input.ControllerName;
+
+            return
+    $@"[HttpPost(""{input.RouteName}"")]
+        public {asyncKeyword}<{input.ReturnType}> {methodName}([{input.ParameterSource}] {input.ParameterType} {input.ParameterName})
+        {{
+    // Your logic here
+        return {(input.IsAsync ? "await Task.FromResult(default(" + input.ReturnType + "))" : "Ok()")};
+        }}";
+        }
+    }
+
+}
